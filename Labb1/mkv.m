c9da9bd2a30b982a8ef59f9919fcc32a3c22cc4c
toshi <toshi.sakao.r@gmail.com>
2024-01-24 20:28:31 +0100

    hatar

--- Labb1/mkv.m
+++ Labb1/mkv.m
@@ -1,6 +1,17 @@
 load STHLMARLANDA2023.mat
 
-T = @(c, t, ts) c(1) + c(2)*t + c(3)*cos(w*ts)*sin(w*t) - c(3)*sin(w*ts)*cos(w*t);
+w = 2*pi / (365*24);
 
-ettor = ones(size(x))
-Td
+Tori = @(c, t, ts) c(1) + c(2)*t + c(3)*sin(w*(t-ts));
+Tnew = @(c, t, ts) c(1) + c(2)*t + c(3)*cos(w*ts)*sin(w*t) - c(3)*sin(w*ts)*cos(w*t);
+
+ts = 0
+t = [0:size(Td)-1]';
+A = [ones(size(t)), t, sin(w*t), cos(w*t)];
+
+c = A\ Td
+
+hold on 
+plot(t, Td, '.')
+plot(t, Tori(c, t, ts))
+plot(t, Tnew(c, t, ts))
